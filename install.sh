b7161dfd230e0dccc962493e59d2dc0537e8ccc2
Ronghua <57265152+66Ronghua99@users.noreply.github.com>
2024-10-13 11:26:23 +0800

    Update install.sh
    Signed-off-by: Ronghua <57265152+66Ronghua99@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 apt update
+# zsh  git curl
+sudo apt install zsh git curl -y
+sudo wget -O $ZSH_CUSTOM/themes/haoomz.zsh-theme https://cdn.haoyep.com/gh/leegical/Blog_img/zsh/haoomz.zsh-theme
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
 # Full Latex environemnt
-apt install texlive texlive-latex-extra texlive-science texlive-extra-utils texlive-full
+apt install texlive texlive-latex-extra texlive-science texlive-extra-utils texlive-full -y
+
+
+
 
