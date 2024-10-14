059c53a202e7e4860fb49014f1b3c16643519b97
Ronghua <57265152+66Ronghua99@users.noreply.github.com>
2024-10-14 19:54:01 +0800

    Update install.sh
    Signed-off-by: Ronghua <57265152+66Ronghua99@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -4,9 +4,18 @@ sudo apt install zsh git curl -y
 sudo wget -O $ZSH_CUSTOM/themes/haoomz.zsh-theme https://cdn.haoyep.com/gh/leegical/Blog_img/zsh/haoomz.zsh-theme
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Anaconda
+apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+curl -O https://repo.continuum.io/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+zsh Anaconda3-2024.06-1-Linux-x86_64.sh
 
 # Full Latex environemnt
 apt install texlive texlive-latex-extra texlive-science texlive-extra-utils texlive-full -y
+
+# node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+nvm install --lts
+
 
 
 
