1db2b179d6bc514ee994a537a53b78e809124373
Ronghua <57265152+66Ronghua99@users.noreply.github.com>
2024-10-30 19:56:50 +0800

    Update install.sh
    Signed-off-by: Ronghua <57265152+66Ronghua99@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -18,6 +18,30 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 nvm install --lts
 
 
+# vscode-fuzzy search
+sudo apt install bat ripgrep fzf
+# to .zshrc
+export FZF_DEFAULT_OPTS="--history=$HOME/.fzf-history"
 
+# exec file to /usr/local bin
 
+#!/usr/bin/env zsh
+##
+# Interactive search.
+#
+[[ -n $1 ]] && cd "$1" # go to provided folder or noop
+
+export FZF_DEFAULT_COMMAND="rg --column --line-number --no-heading --color=always -- ''"
+selected=$(
+  fzf \
+    --ansi \
+    --delimiter : \
+    --bind "f12:execute-silent:(code -g $PWD/{1..3})" \
+    --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+    --preview 'bat -f --highlight-line={2} {1}' | cut -d":" -f1,2,3
+)
+
+[[ -n $selected ]] && code -g "$PWD"/"$selected"
+
+sudo chmod +x /usr/local/bin/rgf
 
